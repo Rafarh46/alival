21d571297e6fadc94373289e69a16e2228648858
Romero <Rafael.Romero01@nemak.com>
2025-04-10 15:22:08 -0600

    commit con stripe

--- backend/server.js
+++ backend/server.js
@@ -62,7 +62,7 @@ app.post('/api/create-checkout-session', async (req, res) => {
 });
 
 // Iniciar servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
